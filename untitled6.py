78c68a322c53440fe344132fb52514ac54ccaacf
MaxLee77 <100952237+MaxLee77@users.noreply.github.com>
2022-07-01 22:05:46 +0000

    yangi file qo'shildi
    juft toq aniqlash

---
+++ untitled6.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 22:11:23 2022
+
+@author: msii
+"""
+
+#num = int(input("Juft son kiriting"))
+#if num %2:
+ #   print("Bu son juft emas")
+#else:
+ #   print("Rahmat.!")
+ 
+#old = int(input("Yoshingizni kiriting: "))
+#if old <= 4 or old >= 60:
+ #   print("Sizga kirish bepul. :)")
+#elif old <= 18:
+ #   print("Sizga kirish 10000 so'm")
+#else:
+ #   print("Sizga kirish 20000 so'm")`
+ 
+#num_a = int(input("Ikkita son kiriting: "))
+#num_b = int(input())
+#n1 = "birnchi son"
+#n2 = "ikkinchi son"
+#if num_a == num_b:
+ #   print(f"{n1} {n2}ga teng. ")
+#elif num_a < num_b:
+ #   print(f"{n1} {n2}dan kichik. ")
+#elif num_a > num_b:
+ #   print(f"{n1} {n2}dan katta. ")
+  
+product = ["Tomato", "Pepper", "Potato", "Carrot", "Cucumber", "Meat", "milk","Pumpkin", "Oil"]
+savat = input("kamida 5 ta mahsulot kiriting")
+if savat in product:
+    print("")
+
+
+  
